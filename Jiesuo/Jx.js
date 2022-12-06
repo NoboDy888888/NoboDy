32de58dcaf62e6c991370d8f93866b199eeccdaf
NoboDy888888 <117597264+NoboDy888888@users.noreply.github.com>
2022-12-06 18:50:36 +0800

    Update Jx.js

--- Jiesuo/Jx.js
+++ Jiesuo/Jx.js
@@ -7,13 +7,4 @@
 
 ^https?:\/\/api\.tipsoon\.com\/api\/v1\/top\/ad url reject-img
 
-^https:\/\/api\.tipsoon\.com\/api\/v1\/user\/info url script-response-body https://github.com/NoboDy888888/NoboDy/main/Jiesuo/Jx.js
-[mitm] 
-
-hostname = api.tipsoon.com
-
-*******************************/
-
-body = $response.body.replace(/\"is_vip":\w+/g, '\"is_vip":true')
-
-$done({body});
+^https:\/\/api\.tipsoon\.com\/api\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/NoboDy888888/NoboDy/main/Jiesuo/Jx.js
