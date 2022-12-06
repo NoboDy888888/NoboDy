41ba2300f29eaee2a1764abb430246be2fa8ca49
NoboDy888888 <117597264+NoboDy888888@users.noreply.github.com>
2022-12-06 20:07:31 +0800

    Update Xmly.js

--- Jiesuo/Xmly.js
+++ Jiesuo/Xmly.js
@@ -14,7 +14,7 @@ TG频道群：https://t.me/WeiRenQAQ
 
 [rewrite_local]
 
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+))mobile-play.+$ url script-request-header https://raw.githubusercontent.com/NoboDy888888/NoboDy/main/Jiesuo/Xmly.js
+^http[s]?:\/\/.+ximalaya.+(product/info|/mobile-user/v2/homePage|/vip/v1/recommand/ts|mobile-playpage/playpage/tabs|/mobile-album/album/page/ts|mobile/v1/album/track/ts|product/promotion/v./whole/album/\d+/price/dynamic/ts|business-vip-presale-mobile-web/page/ts).*$ url script-response-body https://raw.githubusercontent.com/NoboDy888888/NoboDy/main/Jiesuo/Xmly.js
 
 [mitm]
 hostname = *.ximalaya.*
